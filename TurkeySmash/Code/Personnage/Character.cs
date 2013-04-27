3bf22b27a814fb0be4b49eaf7bd08f4612db3bf6
Malstrom <blaise.fulpin@gmail.com>
2013-04-27 16:23:09 +0200

    Combat

--- TurkeySmash/Code/Personnage/Character.cs
+++ TurkeySmash/Code/Personnage/Character.cs
@@ -6,6 +6,8 @@ using FarseerPhysics.Dynamics;
 using FarseerPhysics.Factories;
 using FarseerPhysics.Collision;
 using FarseerPhysics.Dynamics.Contacts;
+using FarseerPhysics.Collision.Shapes;
+using System;
 
 namespace TurkeySmash
 {
@@ -13,7 +15,7 @@ namespace TurkeySmash
     class Character : PhysicsObject
     {
 
-        int vie = 5;
+        public int vie = 5;
         bool canJump;
         World world;
 
@@ -29,29 +31,42 @@ namespace TurkeySmash
 
         public override void Update(GameTime gameTime)
         {
+            //Right & Left
             int forcePower = 6;
             Vector2 force = Vector2.Zero;
-
             if (Keyboard.GetState().IsKeyDown(Keys.Left))
                 force.X = -forcePower;
             if (Keyboard.GetState().IsKeyDown(Keys.Right))
                 force.X = forcePower;
-
             body.ApplyForce(force, body.Position);
 
+            //Jump
             body.OnCollision += bodyOnCollision;
-            world.Step(1 / 300);
+            world.Step(1 / 30);
             if (canJump & Keyboard.GetState().IsKeyDown(Keys.Space))
             {
-                body.ApplyForce(-Vector2.UnitY *100);
+                body.ApplyForce(-Vector2.UnitY * 100);
                 canJump = false;
             }
 
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                Body hit = BodyFactory.CreateRectangle(world, ConvertUnits.ToSimUnits(sprite.Width), ConvertUnits.ToSimUnits(sprite.Height), 1);
+                hit.BodyType = BodyType.Kinematic;
+                hit.Position = new Vector2(bodyPosition.X + sprite.Width, bodyPosition.Y);
+                hit.OnCollision += hitOnColision;
+            }
         }
 
-        private bool bodyOnCollision (Fixture fixA,Fixture fixB, Contact contact)
+        private bool bodyOnCollision(Fixture fixA, Fixture fixB, Contact contact)
         {
             canJump = true;
+            return true;
+        }
+
+        public bool hitOnColision(Fixture fixA, Fixture fixB, Contact contact)
+        {
+            fixB.Body.ApplyLinearImpulse(new Vector2(2.0f, 1.0f));
             return true;
         }
     }
