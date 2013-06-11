e19592e9e3b0a36bbe3a75e6ccc2c8699f0de1c6
Malstrom <blaise.fulpin@gmail.com>
2013-06-11 20:40:24 +0200

    lol

--- TurkeySmash/Code/Menu/SelectionPersonnages.cs
+++ TurkeySmash/Code/Menu/SelectionPersonnages.cs
@@ -9,9 +9,9 @@ namespace TurkeySmash
     {
         #region Fields
 
-        private BoutonImage bouton1;
-        private BoutonImage bouton2;
-        private BoutonTexte bouton3;
+        private BoutonImage boutonNaruto;
+        private BoutonImage boutonSakura;
+        private BoutonTexte boutonRetour;
         public static string persoSelect;
 
         #endregion
@@ -20,20 +20,20 @@ namespace TurkeySmash
 
         public SelectionPersonnage()
         {
-            bouton1 = new BoutonImage();
-            bouton2 = new BoutonImage();
-            bouton3 = new BoutonTexte(TurkeySmashGame.manager.PreferredBackBufferWidth / 2, 700);
-            bouton3.Texte = "Retour";
+            boutonNaruto = new BoutonImage();
+            boutonSakura = new BoutonImage();
+            boutonRetour = new BoutonTexte(TurkeySmashGame.manager.PreferredBackBufferWidth / 2, 700);
+            boutonRetour.Texte = "Retour";
         }
 
         public override void Init()
         {
             backgroundMenu.Load(TurkeySmashGame.content, "Menu\\MenuPrincipal");
-            bouton1.Load(TurkeySmashGame.content, "Menu\\BoutonNaruto", boutons);
-            bouton1.Position = new Microsoft.Xna.Framework.Vector2(TurkeySmashGame.manager.PreferredBackBufferWidth / 3, TurkeySmashGame.manager.PreferredBackBufferHeight / 2);
-            bouton2.Load(TurkeySmashGame.content, "Menu\\BoutonSakura", boutons);
-            bouton2.Position = new Microsoft.Xna.Framework.Vector2(2* TurkeySmashGame.manager.PreferredBackBufferWidth / 3, TurkeySmashGame.manager.PreferredBackBufferHeight / 2);
-            bouton3.Load(TurkeySmashGame.content, boutons);
+            boutonNaruto.Load(TurkeySmashGame.content, "Menu\\BoutonNaruto", boutons);
+            boutonNaruto.Position = new Microsoft.Xna.Framework.Vector2(TurkeySmashGame.manager.PreferredBackBufferWidth / 3, TurkeySmashGame.manager.PreferredBackBufferHeight / 2);
+            boutonSakura.Load(TurkeySmashGame.content, "Menu\\BoutonSakura", boutons);
+            boutonSakura.Position = new Microsoft.Xna.Framework.Vector2(2* TurkeySmashGame.manager.PreferredBackBufferWidth / 3, TurkeySmashGame.manager.PreferredBackBufferHeight / 2);
+            boutonRetour.Load(TurkeySmashGame.content, boutons);
         }
 
         public override void Bouton1()
