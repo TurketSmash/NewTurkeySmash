1481a35ac524651cefdc7e7eead02bf444dc4f2c
Hughlekrogan <hugo.maurer@hotmail.fr>
2013-06-12 15:28:37 +0200

    IA mongolo
    Elle tape derriere elle comme une paibronne

--- TurkeySmash/Code/Personnage/IA.cs
+++ TurkeySmash/Code/Personnage/IA.cs
@@ -14,8 +14,119 @@ namespace TurkeySmash
 {
     class IA : Character
     {
-        public IA(World world, Vector2 position, float density, Vector2 bodySize, PlayerIndex playerindex, AnimatedSpriteDef definition)
-            : base(world, position, density, bodySize, playerindex, definition) { }
+        bool canAction = false;
+        bool fuir = false;
+        bool booleenDeLaFamille = true;
+        int compteur = 0;
+
+
+        public IA(World world, Vector2 position, float density, Vector2 bodySize, PlayerIndex player, AnimatedSpriteDef definition)
+            : base(world, position, density, bodySize, player, definition)
+        { }
+
+        Vector2[] posPerso = new Vector2[4];
+
+
+        public void UpdatePosition(Character[] personnages)
+        {
+            int i = 0;
+            foreach (Character perso in personnages)
+            {
+                if ((perso != null) && (playerindex != perso.playerindex))
+                {
+                    posPerso[i] = perso.bodyPosition;
+                    i = i + 1;
+                }
+            }
+        }
+
+
+        public override void Update(GameTime gameTime)
+        {
+            booleenDeLaFamille = false;
+            if (compteur < 50)
+                compteur = compteur + 1;
+            else
+            {
+                compteur = 0;
+                booleenDeLaFamille = true;
+            }
+
+            float positionXLaPlusProche = 99999; //chiffre assez grand pour qu'il soit modifié des le premier "mecton" du foreach qui suit.
+            float positionYLaPlusProche = -1;
+
+            foreach (Vector2 position in posPerso)
+            {
+                if (position != null)
+                {
+                    if ((position.X < positionXLaPlusProche) && (position.X != 0))
+                    {
+                        positionXLaPlusProche = position.X;
+                        positionYLaPlusProche = position.Y;
+                    }
+                }
+            }
+
+            if (fuir == false)
+            {
+                if (positionXLaPlusProche < (bodyPosition.X) - 1)
+                {
+                    lookingRight = false;
+                    isMoving = true;
+                }
+                else
+                    if (positionXLaPlusProche > (bodyPosition.X) + 1)
+                    {
+                        lookingRight = true;
+                        isMoving = true;
+                    }
+                    else
+                        isMoving = false;
+            }
+
+            //Jump
+
+            jump = ((positionYLaPlusProche < bodyPosition.Y - 0.7) && (booleenDeLaFamille));
+
+
+            //Attack
+
+            if ((bodyPosition.X <= positionXLaPlusProche + 0.7) && (bodyPosition.X >= positionXLaPlusProche - 0.7) && (fuir == false))
+                canAction = true;
+            
+            action = canAction;
+            if (action)
+            {
+                canAction = false;
+                fuir = true;
+            }
+
+            if (fuir)
+            {
+                if ((bodyPosition.X < positionXLaPlusProche + 1.7) && (bodyPosition.X > positionXLaPlusProche))
+                {
+                    lookingRight = true;
+                    isMoving = true;
+                }
+                else
+                    if ((bodyPosition.X > positionXLaPlusProche - 1.7) && (bodyPosition.X < positionXLaPlusProche))
+                    {
+                        lookingRight = false;
+                        isMoving = true;
+                    }
+                    else
+                        isMoving = false;
+            }
+
+            if ((fuir == true) && ((bodyPosition.X > positionXLaPlusProche + 1.7) || (bodyPosition.X < positionXLaPlusProche - 1.7)))
+            {
+                fuir = false;
+            }
+
+            Console.WriteLine(lookingRight);
+
+            base.Update(gameTime);
+        }
 
 
     }
