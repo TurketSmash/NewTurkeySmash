c1e63518cc3a42436f2ecca3d48bc4e313f1cb2b
Malstrom <blaise.fulpin@gmail.com>
2013-06-13 18:03:25 +0200

    Convert playerindex

--- TurkeySmash/Code/Divers/ConvertPlayerIndex.cs
+++ TurkeySmash/Code/Divers/ConvertPlayerIndex.cs
@@ -8,7 +8,7 @@ namespace TurkeySmash
 {
     public static class Convert
     {
-        public static int playerIndex(PlayerIndex index)
+        public static int PlayerIndex2Int(PlayerIndex index)
         {
             switch (index)
             {
@@ -21,7 +21,24 @@ namespace TurkeySmash
                 case PlayerIndex.Four:
                     return 4;
             }
+            Console.WriteLine("Erreur <PlayerIndex2Int> : Erreur index.");
             return -1;
+        }
+        public static PlayerIndex Int2PlayerIndex(int integer)
+        {
+            switch (integer)
+            {
+                case 1:
+                    return PlayerIndex.One;
+                case 2:
+                    return PlayerIndex.Two;
+                case 3:
+                    return PlayerIndex.Three;
+                case 4:
+                    return PlayerIndex.Four;
+            }
+            Console.WriteLine("Erreur <Int2PlayerIndex> : int != 1,2,3 ou 4. (PlayerIndex.One returned)");
+            return PlayerIndex.One;
         }
     }
 }
